a816a400491bc5e3087bc349af1e583cb91c117b
Bisrat Gebrekidan <github@bisrat.dev>
2021-03-24 17:12:41 -0600

    Simplified GUI

--- src/Chess.cpp
+++ src/Chess.cpp
@@ -7,7 +7,7 @@ int main(){
     string s;
     while(1){
         b.playGame();
-        cout << "Play again?\n[1] Yes\nAny key to exit\n > ";
+        cout << "Play again?\n[1] Yes\n[Any] Exit\n > ";
         getline(cin, s, '\n');
         if(s != "1") break;
     }
