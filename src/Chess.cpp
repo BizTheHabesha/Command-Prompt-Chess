96fee1d7cdf4e34c6b13d483a94ca29527cffdb6
Bisrat Gebrekidan <github@bisrat.dev>
2021-03-24 14:49:55 -0600

    y/n functionality

--- src/Chess.cpp
+++ src/Chess.cpp
@@ -3,5 +3,14 @@
 using namespace std;
 
 int main(){
+    Board b;
+    string s;
+    while(1){
+        b.playGame();
+        cout << "Play again?\n[1] Yes\n > ";
+        cin >> s;
+        if(s != "1") break;
+    }
+    system("CLS");
     return 0;
 }
